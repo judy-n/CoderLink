cf0ed12adde1dc4f5cda388bd5573d2577e24855
judy-n <judynaamani@icloud.com>
2021-10-28 19:29:28 -0400

    profile sample

--- src/views/profilePage/index.js
+++ src/views/profilePage/index.js
@@ -1,14 +1,38 @@
 
 import React from 'react';
+import "./styles.css";
+import profilepic from "./static/default.jpeg"
 
 class Profile extends React.Component {
 
-    state = {
-        username: ""
+    state = { // we will load data into state variables
+        name: "Judy Naamani",
+        bio: "Hi I'm a silly goose!",
+        institution: "University of Toronto",
+        skills: ["python", "java"],
     }
     render() { 
         return (
-        <div>Profile Page {this.props.username}</div>
+        <div>
+        
+        <h1>Profile of {this.props.username}</h1>
+        
+        <div className='card'>
+            {/* Profile pic */}
+            <img src={profilepic} alt=""></img>
+            <div className='about'>
+                <h3>Name: <span className="content">{this.state.name}</span></h3>
+                <h3>Bio: <span className="content">{this.state.bio}</span></h3>
+                <h3>Institution: <span className="content">{this.state.institution}</span></h3>
+                <h3>Skills: {this.state.skills.map(skill => <span className="badge">{skill}</span>)}</h3>
+                <div className="about-buttons">
+                  <button onClick={() => {}} className="about-btn blue">Connect</button>
+                  <button onClick={() => {}} className="about-btn green">Portfolio</button>
+                </div>
+            </div>
+        </div>
+                
+        </div>
         );
     }
 }
