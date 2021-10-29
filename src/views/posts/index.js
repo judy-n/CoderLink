b210907609f65dead1f5221ebc0a87a5910c3ffc
Cecil Cao <pokemonball34@gmail.com>
2021-10-29 02:11:20 -0400

    Cleaned up the GUI of a post.

--- src/views/posts/index.js
+++ src/views/posts/index.js
@@ -1,17 +1,17 @@
+'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
 import Header from '../../components/Header';
-
-
-
-
+import Button from '@mui/material/Button';
+import profilepic from '../profilePage/static/default.jpeg';
+import banner from './static/banner.jpg';
 
 
 class Post extends React.Component {
     
     state = {
-        // State variables that will be used to replace the dummy placeholders
+        // State variables that will be used as dummy placeholders
         poster_username: "cecil_c",
         institution: "UofT",
         postTitle: "A Latin Compilation Project",
@@ -20,47 +20,54 @@ class Post extends React.Component {
     
     render() {
         return (
-           <div className="postPage">
+            <div className="postPage">
                 <Header/>
             
             {/*Contains information about the poster and the date they posted.*/}
-            <div className="postHeaderContainer">
-                <span className="profilePicture"><img src={profilepic} alt=""></img></span>
-                <h4><span className="username">@{this.state.poster_username}</span></h4>
-            
-                {/* TODO: Make the date readable! */}
-                <span className="datePosted">
-                    {this.state.datePosted}
-                </span>
+            <div className="leftSide">
+                <div className="postHeaderContainer">
+                    <span className="profilePicture"><img src={profilepic} alt=""></img></span>
+                    <h4><span className="username">@{this.state.poster_username}</span></h4>
+
+                    {/* TODO: Make the date readable! */}
+                    <span className="datePosted">
+                        {this.state.datePosted}
+                    </span>
+                </div>
+
+                {/*A decorative banner, should the poster wish to upload one.*/}
+                <div className="postBanner">
+                    <img src={banner} alt=""></img>
+                </div>
+
+                {/* The post's title page */}
+                <div className="postTitle">
+                    <h2>{this.state.postTitle}</h2>
+                </div>
+
+
+                {/*The main text, explaining what the side-project is about.*/}
+                <div className="postText">
+                    <p>Elapsam semel occasionem non ipse potest Iuppiter reprehendere. Our goal is to encode many great latin phrases like this one into one giant encyclopedia for all to use!</p>
+                </div>
+
+
+                {/* Button for applying for this side-project. */}
+                <Button onClick={() => {}} variant="primary" size="lg" enabled className="applyButton">
+                    Apply Now
+                </Button>
             </div>
-            
-            {/*A decorative banner, should the poster wish to upload one.*/}
-            <div className="postBanner">
-                <img src={banner} alt=""></img>
+
+            <div className="rightSide">
+                {/* Relevant tags that the poster added.*/}
+                <div className="tagList">
+                    <h3>Relevant Tags</h3>
+                    <ol>Latin</ol>
+                    <ol>HTML/CSS</ol>
+                    <ol>JavaScript</ol>
+                </div>
+
             </div>
-            
-            <div className="postTitle">
-                <h2>{this.state.postTitle}</h2>
-            </div>
-            
-            
-            {/*The main text, explaining what the side-project is about.*/}
-            <div className="postText">
-                <p>Elapsam semel occasionem non ipse potest Iuppiter reprehendere.</p>
-            </div>
-            
-            {/* Relevant tags that the poster added.*/}
-            <div className="tagList">
-                <h3>Relevant Tags</h3>
-                <ul>Latin</ul>
-                <ul>HTML/CSS</ul>
-                <ul>JavaScript</ul>
-            </div>
-            
-            <button onClick={() => {}} variant="primary" size="lg" disabled className="applyButton">
-                Apply Now
-            </button>
-            
             </div>  
         );
     }
