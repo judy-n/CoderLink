69d92de18edc74b23e81dd7ac8fae05e45e32f65
akshatayush <akshatayush07@gmail.com>
2021-11-07 15:42:00 -0500

    added card to homscreen

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Post from './views/posts/index';
 import Portfolio from './views/Portfolio';
 import NewPostPage from './views/newPostPage';
 import UserEntity from './model/User';
+import PostEntity from './model/Post';
 
 import { addPost, getUserPosts, removePost } from './actions/PostListActions';
 import { addUser, removeUser, getUser } from './actions/UserListActions';
@@ -40,6 +41,33 @@ class App extends React.Component {
       userList: [newUser],
       currentUser: newUser
     });
+    const newPost1 = new PostEntity('admin');
+    newPost1.addInformation(
+      'Recommender system for cafes',
+      'Using machine mearning to recommend cafes to users based on their previous choices',
+      '',
+      'University of Toronto',
+      ['Python', 'Deep learning']
+    );
+    this.addPost(newPost1);
+    const newPost2 = new PostEntity('johnny');
+    newPost2.addInformation(
+      'A game engine in C',
+      'Designing a game engine which would provide anime style physics for a game I am working on',
+      '',
+      'University of Toronto',
+      ['C', 'Linear algebra', 'Vector calculus']
+      );
+    this.addPost(newPost2);
+    const newPost3 = new PostEntity('chloe');
+    newPost3.addInformation(
+      'An application to support the underprivileged',
+      'We need a flutter developer for our application which aims to help the homeless and needy',
+      '',
+      'University of Waterloo',
+      ['Flutter', 'Figma']
+    );
+    this.addPost(newPost3);
   }
 
   handleLogin(username, password) {
--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -3,44 +3,12 @@ import './style.css';
 import PostCard from '../PostCard';
 import { uid } from 'react-uid';
 import ToolBar from '../ToolBar';
+import PostThumbnail from '../PostThumbnail';
+import coffee from './static/coffee.jpg'
 
 import PostEntity from '../../model/Post';
 
-// class Post {
-//     constructor(username, projectTitle, description, institution, skills) {
-//         this.username = username;
-//         this.projectTitle = projectTitle;
-//         this.description = description;
-//         this.institution = institution;
-//         this.skills = skills;
-//     }
-// }
-
 class PostList extends React.Component {
-    // state = {
-    //     postList: [
-    //         new Post('Jake',
-    //                 'Recommender system for cafes',
-    //                 'Building a recommender system to recomment cafes based on previous food likings.',
-    //                 'University of Toronto',
-    //                 ['Python', 'Neural networks']),
-    //         new Post('Nina',
-    //                 'RPG game',
-    //                 'Need someone proficient in C++ for my open world rpg game project',
-    //                 'Independent',
-    //                 ['C++', 'Unreal engine'])
-    //     ]
-    // }
-
-    // addPost = (username, projectTitle, description, institution, skills) => {
-    //     const newPostList = this.state.postList;
-    //     const newPost = new Post(username, projectTitle, description, institution, skills);
-    //     newPostList.push(newPost);
-    //     this.setState({
-    //         postList: newPostList
-    //     })
-    // }
-
     render() {
         return (
             <div>
@@ -50,12 +18,14 @@ class PostList extends React.Component {
                 <div className='postList'>
                     {this.props.postList.map((post) => {
                         return (
-                            <PostCard key={uid(post)}
+                            <PostCard
+                                key={uid(post)}
                                 username={post.author}
-                                projectTitle={post.title}
+                                projectTitle = {post.title}
                                 description={post.descriptionShort}
-                                institution={post.institutions[0]}
-                                skills={post.skillsRequired}
+                                skills ={post.skillsRequired}
+                                banner={coffee}
+                                institution = {post.institutions[0]}
                             />
                         );
                     })}
