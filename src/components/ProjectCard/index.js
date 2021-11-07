87e137bf98c835d764239aff7aab370d74ae5bc9
judy-n <judynaamani@icloud.com>
2021-11-07 00:34:38 -0400

    portfolio projects

---
+++ src/components/ProjectCard/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+import IconButton from '@mui/material/IconButton';
+import LinkIcon from '@mui/icons-material/Link';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'
+
+
+class ProjectCard extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: this.props.title,
+            description: this.props.description,
+            // skills: this.props.skills,
+            banner: this.props.banner,
+            // githubLink: this.props.githubLink,
+            // projLink: this.props.projLink
+
+        }
+    }
+
+    render() { 
+        return (<div>
+
+<div className="port-proj-card">
+                <Card>
+                    <CardMedia
+                        component="img"
+                        image={this.state.banner}
+                        height="140"
+                   />
+                   <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                    {this.state.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                     {this.state.description}
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                    <IconButton aria-label="github repo">
+                    <GitHubIcon />
+                    </IconButton>
+                    <IconButton aria-label="link">
+                    <LinkIcon />
+                    </IconButton>
+                    </CardActions>
+                </Card>
+                </div>
+
+
+
+        </div>
+        );
+    }
+}
+ 
+export default ProjectCard;
--- src/views/Portfolio/index.js
+++ src/views/Portfolio/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import "./style.css";
 import Header from '../../components/Header';
 import profilepic from "./static/default.jpeg"
+import IconButton from '@mui/material/IconButton';
+import LinkIcon from '@mui/icons-material/Link';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import github from './static/github.png';
+import resume from './static/resume.png';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'
+import pixel from './static/pixel.png'
+import shooter from './static/shooter.png'
+import ProjectCard from '../../components/ProjectCard'
+
 
 class Portfolio extends React.Component {
 
@@ -18,11 +33,41 @@ class Portfolio extends React.Component {
             
             <div className="port-profile">
                 <img src={profilepic}></img>
-                <h3>{this.state.name}</h3>
+                <div className="port-name">
+                <h4>{this.state.name}</h4>
+                <div className="port-icons">
+                <a href="#"><img src={github}></img></a>
+                <a href="#"><img src={resume}></img></a>
+                </div>
+                </div>
             </div>
             
             </div>
-            <div className="port-projects">hello</div>
+    
+
+            <div className="port-projects">
+            <ProjectCard
+
+            title="Vardew Stalley"
+            banner={pixel}
+            description="A farming RPG game for PC, final project for a university course."
+            />
+
+            <ProjectCard
+
+            title="Forkknife"
+            banner={shooter}
+            description="Online multiplayer shooter game that I made for a hackathon."
+            />
+
+            <ProjectCard
+
+            title="Vardew Stalley"
+            banner={pixel}
+            description="A farming RPG game for PC, final project for a university course."
+            />
+                
+            </div>
 
 
 
