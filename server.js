79549aa9128d8ca1cead350a2385111a80a03dd4
judy-n <judynaamani@icloud.com>
2021-12-04 03:28:43 -0500

    make sessions 10hrs long

--- server.js
+++ server.js
@@ -79,13 +79,13 @@ app.use(
         resave: false,
         saveUninitialized: false,
         cookie: {
-            expires: 60000,
+            expires: 36000000,
             httpOnly: true
         },
         // store the sessions on the database in production
         store: env === 'production' ? MongoStore.create({
                                                 mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/TeamFinderAPI'
-                                 }) : null
+                                 }) : MongoStore.create({mongoUrl: 'mongodb://localhost:27017/TeamFinderAPI'})
     })
 );
 
